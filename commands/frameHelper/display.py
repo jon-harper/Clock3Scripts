6957df3dbacb911dbf29658ec90868a17b818bbc
Jon Harper <mnijph.cpp@gmail.com>
2022-03-31 09:56:37 -0500

    Refactoring

--- commands/frameHelper/display.py
+++ commands/frameHelper/display.py
@@ -4,7 +4,7 @@ import adsk.core
 import adsk.fusion
 from ...lib import fusion360utils as futil
 
-def get_occurrences() -> adsk.fusion.OccurrenceList:
+def get_all_occurrences() -> adsk.fusion.OccurrenceList:
     app = adsk.core.Application.get()
     ui = app.userInterface
 
@@ -17,7 +17,7 @@ def get_occurrences() -> adsk.fusion.OccurrenceList:
 
 def show_tee_nut_view() -> None:
     futil.log('show_tee_nut_view()_ called')
-    items = get_occurrences()
+    items = get_all_occurrences()
     for item in items:
         comp = item.component
         pn: str = comp.partNumber
@@ -30,7 +30,7 @@ def show_tee_nut_view() -> None:
 
 def show_frame_view() -> None:
     futil.log('show_frame_view()_ called')
-    items = get_occurrences()
+    items = get_all_occurrences()
     for item in items:
         comp = item.component
         pn: str = comp.partNumber
@@ -43,5 +43,5 @@ def show_frame_view() -> None:
 
 def show_all():
     futil.log('show_all()_ called')
-    for item in get_occurrences():
+    for item in get_all_occurrences():
         item.isLightBulbOn = True
